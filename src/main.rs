d63b0cc234360573765d8eaf6d0a8f19647b3d7a
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-07 03:21:56 +0000

    docs: Add styling to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,76 @@
 mod types;
 mod commands;
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use types::DependencyGraph;
-use std::{path::PathBuf, time::Instant};
-use log::{debug, LevelFilter};
+use std::{fs::File, io::BufReader, path::PathBuf};
+use commands::Commands;
+
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(
+    version, 
+    about, 
+    long_about = None,
+    color = clap::ColorChoice::Auto,
+    styles = get_styles()
+)]
 struct Cli {
     /// Optional name to operate on
+    #[arg(help_heading = "OPTIONS")]
     name: Option<String>,
 
     /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
+    #[arg(
+        short, 
+        long, 
+        value_name = "FILE",
+        help_heading = "OPTIONS"
+    )]
     config: Option<PathBuf>,
 
     /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
+    #[arg(
+        short, 
+        long, 
+        action = clap::ArgAction::Count,
+        help_heading = "GLOBAL FLAGS"
+    )]
     debug: u8,
 
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    /// Prepares the dependency graph from the given files
-    Prepare {
-        /// The directory to recursively scan for files
-        #[arg(short, long, value_name = "DIR")]
-        dir: PathBuf,
-        /// The name of the dependency toml file commmon to all the services. Defaults to `dependencies.toml`
-        #[arg(long, value_name = "NAME")]
-        dependency_toml_name: Option<String>,
-    },
-    /// Queries the dependency graph
-    Query {
-        /// The JSON artifact containing the previously prepared dependency graph from the `prepare` command
-        #[arg(short, long, value_name = "FILE")]
-        graph_artifact: String,
-        /// A list of file paths to query
-        #[arg(short, long, value_name = "FILE")]
-        files: Vec<PathBuf>,
-    },
+pub fn get_styles() -> clap::builder::Styles {
+    use clap::builder::styling::{Style, Color, AnsiColor};
+    clap::builder::Styles::styled()
+        .header(Style::new().bold().fg_color(Some(Color::Ansi(AnsiColor::Green))))
+        .usage(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
+        .literal(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
+        .placeholder(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Yellow))))
+        .error(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Red))))
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let log_level = match cli.debug {
-        0 => LevelFilter::Warn,
-        1 => LevelFilter::Info,
-        2 => LevelFilter::Debug,
-        _ => LevelFilter::Trace,
-    };
-
+    // let log_level: LevelFilter = match cli.debug {
+        //     0 => LevelFilter::Warn,
+        //     1 => LevelFilter::Info,
+        //     2 => LevelFilter::Debug,
+        //     _ => LevelFilter::Trace,
+        // };
+        
     env_logger::builder().init();
 
     match cli.command {
         Some(Commands::Prepare { dir, dependency_toml_name }) => {
-            let start = Instant::now();
-            let graph = commands::create_graph_from_dir(dir, dependency_toml_name);
-            let end = start.elapsed();
-            debug!("Time taken to prepare graph: {:?}", end);
             
+            // Prepare the graph object
+            let graph = commands::prepare(dir, dependency_toml_name);
+            
+            // Serialize the graph object to JSON
             match graph {
                 Ok(g) => match serde_json::to_string(&g) {  
                     Ok(json) => println!("{}", json),
@@ -73,17 +79,16 @@ fn main() {
                 Err(e) => println!("Error: {}", e),
             }
         }
-        Some(Commands::Query { graph_artifact, files }) => {
-            let start = Instant::now();
-            let graph: DependencyGraph = serde_json::from_str(&graph_artifact).unwrap();
-            let end = start.elapsed();
-            debug!("Time taken to deserialize graph: {:?}", end);
+        Some(Commands::Query { graph_artifact_path, files }) => {
+            // Read the graph artifact from the file
+            let file = File::open(graph_artifact_path).unwrap();
+            let reader = BufReader::new(file);
+            let graph: DependencyGraph = serde_json::from_reader(reader).unwrap();
 
-            let start = Instant::now();
-            let affected_nodes = commands::get_affected_nodes(&graph, &files);
-            let end = start.elapsed();
-            debug!("Time taken to get affected nodes: {:?}", end);
+            // Query the graph for the given files
+            let affected_nodes = commands::query(&graph, &files);
 
+            // Serialize the affected nodes to JSON
             match serde_json::to_string(&affected_nodes) {
                 Ok(json) => println!("{}", json),
                 Err(e) => println!("Error serializing: {}", e),
